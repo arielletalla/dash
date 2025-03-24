9cf8d3ab6c3c3c9f97d1c0bfbbc735d0683e97b3
arielletalla <tallaarielle3@gmail.com>
2025-03-24 14:21:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2855,5 +2855,5 @@ def update_prediction(n_clicks, age, niveau_etude, genre, situation_mat, deja_do
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8050)
 
